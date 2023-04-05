eae69ed241bc2c54b08932896452be42bf41f385
saleem278 <saleem27898@gmail.com>
2023-04-06 01:44:56 +0530

    ....

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(payPalClientId)
 )
 
+
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
