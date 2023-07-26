69e78b84fa2b81972e17115806145e1da786cf4d
saleem278 <saleem27898@gmail.com>
2023-07-26 07:03:12 +0530

    CORS Issue

--- index.js
+++ index.js
@@ -1,66 +1,59 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import morgan from 'morgan'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import cors from "cors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js';
-import formRoutes from './routes/formRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import formRoutes from "./routes/formRoutes.js";
 
+dotenv.config();
 
-dotenv.config()
+connectDB();
 
-connectDB()
+const app = express();
+const payPalClientId =
+  "AbA_GIL_9JHw3ZjZa3LD7KFgXuaWN9bAzFOhvpHimHcp0BEomjiozAbW5c7frbMuQqfCf9FwsCUzy1zL";
 
-const app = express()
-const payPalClientId = 'AbA_GIL_9JHw3ZjZa3LD7KFgXuaWN9bAzFOhvpHimHcp0BEomjiozAbW5c7frbMuQqfCf9FwsCUzy1zL'
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "API is running...." });
+});
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/form", formRoutes);
+app.get("/api/config/paypal", (req, res) => res.send(payPalClientId));
 
-app.use(express.json())
-app.get("/",(req,res)=>{
-  res.json({"message":"API is running...."})
-})
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
-app.use("/api/form",formRoutes);
-app.get('/api/config/paypal', (req, res) =>
-  res.send(payPalClientId)
-)
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
 }
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running on port 8000`.yellow.bold
-  )
-)
+app.listen(PORT, console.log(`Server running on port 8000`.yellow.bold));
